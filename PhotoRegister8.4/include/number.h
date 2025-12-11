63b69de9904da5a9fa09f26f3ca89c8b986fba94
Suho Kwon <suho.kwon@gmail.com>
2025-12-11 10:55:28 +0900

    Add change.

---
+++ PhotoRegister8.4/include/number.h
@@ -0,0 +1,12 @@
+#ifndef NUMBER_INPUT_H
+#define NUMBER_INPUT_H
+
+#include "types.h"
+
+// ============================================================================
+// NUMBER INPUT SCREEN API
+// ============================================================================
+
+void create_number_input_screen(void);
+
+#endif
--- PhotoRegister8.4/include/number_input.h
+++
@@ -1,6 +0,0 @@
-#ifndef NUMBER_INPUT_H
-#define NUMBER_INPUT_H
-
-void create_number_input_screen(void);
-
-#endif // NUMBER_INPUT_H
